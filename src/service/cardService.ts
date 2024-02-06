810d63c12e574a2dababbdba846787cf4bf15658
Amier Ali <amier_1998@hotmail.com>
2024-02-06 23:56:16 +0000

    feat: display title and id for cards

--- src/service/cardService.ts
+++ src/service/cardService.ts
@@ -4,6 +4,11 @@ import { Card } from "../model/cardData";
 //const cardsDataPath = path.resolve(__dirname, '../data/cards.json');
 
 export function getCardData() {
-  console.log(card);
-  return card;
+  const cardList = card.map((card) => {
+    return {
+      title: card.title,
+      card_id: card.id,
+    };
+  });
+  return cardList;
 }
