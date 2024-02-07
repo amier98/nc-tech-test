50cc9be297b854b96b4aacfaeff352cff308d0c8
Amier Ali <amier_1998@hotmail.com>
2024-02-07 15:15:14 +0000

    feat: Implement getCardData function to return list of cards

--- src/service/cardService.ts
+++ src/service/cardService.ts
@@ -1,12 +1,18 @@
-import path from "path";
 import card from "../data/cards.json";
+import templates from "../data/templates.json";
 import { Card } from "../model/cardData";
-//const cardsDataPath = path.resolve(__dirname, '../data/cards.json');
+import { Template } from "../model/templateData";
 
 export function getCardData() {
-  const cardList = card.map((card) => {
+  const cardList = card.map((card: Card) => {
+    const firstPageTemplateId = card.pages[0].templateId;
+    const template = templates.find(
+      (tem: Template) => tem.id === firstPageTemplateId
+    );
+
     return {
       title: card.title,
+      image_url: template.imageUrl,
       card_id: card.id,
     };
   });
